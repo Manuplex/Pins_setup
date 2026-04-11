543c2eac1f859b49aceb581a34bffa8454da0528
Pierre <pmanou792@gmail.com>
2026-04-11 12:37:13 +0000

    config_url_pattern_polyfill

--- Front/vite.config.ts
+++ Front/vite.config.ts
@@ -3,6 +3,7 @@ import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
+import 'urlpattern-polyfill';
 
 // https://vite.dev/config/
 export default defineConfig({

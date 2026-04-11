71cec8099a8271f6b82a5449efdf327d7ba6f641
Pierre <pmanou792@gmail.com>
2026-04-11 14:35:39 +0000

    pydantic_dummy_var

--- shared/src/shared/config.py
+++ shared/src/shared/config.py
@@ -21,36 +21,37 @@ class Settings(BaseSettings):
     """Configuration typée de l'application."""
 
     # URL de base de l'instance FreshRSS (ex: http://localhost:8080)
-    freshrss_base_url: str
+    freshrss_base_url: str =""
 
     # Clé ou token API FreshRSS (à configurer côté FreshRSS)
-    freshrss_api_token: str
+    freshrss_api_token: str =""
 
     # ID ou nom de la catégorie ciblée
-    freshrss_category_id: str
+    freshrss_category_id: str =""
 
     # DSN de connexion à MariaDB pour SQLAlchemy
     # Exemple : mysql+pymysql://user:password@host:3306/db_name
-    database_url: str
+    database_url: str =""
 
     # Dossier où seront stockées les images téléchargées
-    image_storage_dir: str
+    image_storage_dir: str =""
 
     #api_connexion
-    freshrss_token_greader: str
+    freshrss_token_greader: str =""
 
     #hash _md5
-    Hash_md5:  str
+    Hash_md5:  str =""
 
     #groq_key
-    groq_Api_key: str
+    groq_Api_key: str =""
 
     #Asynchrone_session_db
-    Async_database_url: str
+    Async_database_url: str =""
 
     model_config = SettingsConfigDict(
         env_file=ENV_FILE_PATH,           # Il charge le .env automatiquement
-        env_file_encoding="utf-8"
+        env_file_encoding="utf-8",
+        extra="ignore"
     )
 
 
